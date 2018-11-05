94b1cb9a4b6a8b3128c99dfa5319e99bfbec060a
少年那已不再 <15997352948@163.com>
2018-11-05 16:38:32 +0800

    提交完完整项目

---
+++ src/MvcAlipayStart/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using MvcAlipayStart.Models;
+using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace MvcAlipayStart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/MvcAlipayStart/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcAlipayStart.MvcApplication" Language="C#" %>

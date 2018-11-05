c2cb1c99b4e0e63b4805e341946c2c91cc06a1f0
少年那已不再 <15997352948@163.com>
2018-11-05 18:17:58 +0800

    添加单个的支付宝案例

---
+++ src/AlipayDemo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Diagnostics;
+
+namespace AlipayDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
